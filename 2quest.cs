eb5bbde0181c4542f2c94e43d66a1c99964369c4
schempp21 <74864314+schempp21@users.noreply.github.com>
2020-11-24 19:24:00 +0400

    2 задание
    2 задание

---
+++ 2quest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            try
+            {
+
+                Console.Write("Введите первое число: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                if (y == 0)
+                {
+                    Console.WriteLine("О нет, ты хочешь поделить на ноль? НЕЛЬЗЯ!");
+                }
+                else
+                {
+                    Console.Write("Ответ: ");
+                    Console.WriteLine(x / y);
+                }
+            }
+
+            catch (FormatException)
+            {
+                Console.WriteLine("Введите число!");
+            }
+        }
+    }
+}

364a88ef6fb6037279b56bac33deba5341b345d1
schempp21 <74864314+schempp21@users.noreply.github.com>
2020-11-24 19:39:10 +0400

    3 задание
    3 задание

---
+++ 3quest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char c = Console.ReadLine()[0];
+        Console.WriteLine("Следующий символ: {0}", (char)(c + 1));
+        Console.ReadKey(false);
+    }
+}

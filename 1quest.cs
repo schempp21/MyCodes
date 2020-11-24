1cfb9497c4bb97d3eee30fd0c9083ea76c354507
schempp21 <74864314+schempp21@users.noreply.github.com>
2020-11-24 19:31:54 +0400

    1 задание
    1 задание

---
+++ 1quest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите свое имя: ");
+
+        string name = Console.ReadLine();
+
+        Console.WriteLine($"Привет, {name}");
+    }
+}
